1de4be81c4fb333d9e3f0e2d65bb8a75562c2326
merchantry <fivanovic98@gmail.com>
2023-08-13 20:41:47 +0200

    flattened contracts

---
+++ flattened/AlgebraDEXAdapter.sol
@@ -0,0 +1,250 @@
+// Sources flattened with hardhat v2.17.0 https://hardhat.org
+
+// File contracts/algebra/IAlgebraSwapCallback.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title Callback for IAlgebraPoolActions#swap
+/// @notice Any contract that calls IAlgebraPoolActions#swap must implement this interface
+/// @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
+/// https://github.com/Uniswap/v3-core/tree/main/contracts/interfaces
+interface IAlgebraSwapCallback {
+    /// @notice Called to `msg.sender` after executing a swap via IAlgebraPool#swap.
+    /// @dev In the implementation you must pay the pool tokens owed for the swap.
+    /// The caller of this method must be checked to be a AlgebraPool deployed by the canonical AlgebraFactory.
+    /// amount0Delta and amount1Delta can both be 0 if no tokens were swapped.
+    /// @param amount0Delta The amount of token0 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token0 to the pool.
+    /// @param amount1Delta The amount of token1 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token1 to the pool.
+    /// @param data Any data passed through by the caller via the IAlgebraPoolActions#swap call
+    function algebraSwapCallback(int256 amount0Delta, int256 amount1Delta, bytes calldata data) external;
+}
+
+
+// File contracts/algebra/ISwapRouter.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+/// @title Router token swapping functionality
+/// @notice Functions for swapping tokens via Algebra
+/// @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
+/// https://github.com/Uniswap/v3-periphery
+interface ISwapRouter is IAlgebraSwapCallback {
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint160 limitSqrtPrice;
+    }
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another token
+    /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
+
+    struct ExactInputParams {
+        bytes path;
+        address recipient;
+        uint256 deadline;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+    }
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInput(ExactInputParams calldata params) external payable returns (uint256 amountOut);
+
+    struct ExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+        uint160 limitSqrtPrice;
+    }
+
+    /// @notice Swaps as little as possible of one token for `amountOut` of another token
+    /// @param params The parameters necessary for the swap, encoded as `ExactOutputSingleParams` in calldata
+    /// @return amountIn The amount of the input token
+    function exactOutputSingle(ExactOutputSingleParams calldata params) external payable returns (uint256 amountIn);
+
+    struct ExactOutputParams {
+        bytes path;
+        address recipient;
+        uint256 deadline;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+    }
+
+    /// @notice Swaps as little as possible of one token for `amountOut` of another along the specified path (reversed)
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactOutputParams` in calldata
+    /// @return amountIn The amount of the input token
+    function exactOutput(ExactOutputParams calldata params) external payable returns (uint256 amountIn);
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
+    /// @dev Unlike standard swaps, handles transferring from user before the actual swap.
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInputSingleSupportingFeeOnTransferTokens(
+        ExactInputSingleParams calldata params
+    ) external returns (uint256 amountOut);
+}
+
+
+// File contracts/DEXAdapterHandlerUtils/DEXAdapterInterface.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface DEXAdapterInterface {
+    function getUniswapRouter() external view returns (address);
+
+    function swap(uint256 amountIn, address sendTo, address wTLOS, address forceToken) external returns (uint256);
+}
+
+
+// File contracts/ForceTokenSaleUtils/TimeHandler.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+abstract contract TimeHandler {
+    function time() internal view virtual returns (uint256) {
+        return block.timestamp;
+    }
+}
+
+
+// File contracts/openzeppelin/token/ERC20/IERC20.sol
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.19;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+
+// File contracts/AlgebraDEXAdapter.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+contract AlgebraDEXAdapter is DEXAdapterInterface, TimeHandler {
+    ISwapRouter private uniswapRouter;
+
+    constructor(address _uniswapRouter) {
+        uniswapRouter = ISwapRouter(_uniswapRouter);
+    }
+
+    /**
+     * @dev Returns the address of the SwapRouter contract.
+     */
+    function getUniswapRouter() public view returns (address) {
+        return address(uniswapRouter);
+    }
+
+    /**
+     * @dev Swaps the given amount of wTLOS for FORCE tokens and sends them to the given address.
+     * Must have transferred the given amount of wTLOS to this contract before calling this function.
+     *
+     * @param amountIn The amount of wTLOS to swap.
+     * @param sendTo The address to send the FORCE tokens to.
+     * @param wTLOS The address of the wTLOS token.
+     * @param forceToken The address of the FORCE token.
+     * @return {uint256} amount of FORCE tokens received.
+     */
+    function swap(uint256 amountIn, address sendTo, address wTLOS, address forceToken) external returns (uint256) {
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: wTLOS,
+            tokenOut: forceToken,
+            recipient: sendTo,
+            deadline: time() + 1 days,
+            amountIn: amountIn,
+            amountOutMinimum: 1,
+            limitSqrtPrice: 0
+        });
+
+        IERC20(wTLOS).approve(address(uniswapRouter), amountIn);
+        return uniswapRouter.exactInputSingle(params);
+    }
+}
